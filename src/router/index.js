d454d1ad6e3714a76c3c6d36da0a98a39344033e
Juanco <juaa.espinoza@duocuc.cl>
2025-04-06 21:14:46 -0400

    Hice que el contador y la lista de tareas esten adentro de un sidebar

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView,
+    // },
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/contador',//router link del sidebar, significa lo mismo 
+      //que en name pero este le cambiaron el formato para que se diferencie del otro
+      name: 'Contador',//archivo de components
+
+      component: () => import('../components/Contador.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+      path: '/lista-de-tareas',//router link del sidebar, significa lo mismo 
+      //que en name pero este le cambiaron el formato para que se diferencie del otro
+      name: 'ListaDeTareas',//archivo de components
+
+      component: () => import('../components/ListaDeTareas.vue'),
+    }
   ],
 })
 
