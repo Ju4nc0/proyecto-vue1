a2915dde3c08bf723a74f756ece6ef119b3b3090
Juanco <juaa.espinoza@duocuc.cl>
2025-04-07 20:53:20 -0400

    instala vee-validate, crea modulos

--- src/router/index.js
+++ src/router/index.js
@@ -14,14 +14,21 @@ const router = createRouter({
       //que en name pero este le cambiaron el formato para que se diferencie del otro
       name: 'Contador',//archivo de components
 
-      component: () => import('../components/Contador.vue'),
+      component: () => import('../modules/contador/components/Contador.vue'),
     },
     {
       path: '/lista-de-tareas',//router link del sidebar, significa lo mismo 
       //que en name pero este le cambiaron el formato para que se diferencie del otro
       name: 'ListaDeTareas',//archivo de components
 
-      component: () => import('../components/ListaDeTareas.vue'),
+      component: () => import('../modules/listaDeTareas/components/ListaDeTareas.vue'),
+    },
+    {
+      path: '/registrar',//router link del sidebar, significa lo mismo 
+      //que en name pero este le cambiaron el formato para que se diferencie del otro
+      name: 'registrar',//archivo de components
+
+      component: () => import('../modules/registro/views/Registrarview.vue'),
     }
   ],
 })
