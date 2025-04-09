711ffe4ce9836e123fea23637167e9a7d8f421c1
Juanco <juaa.espinoza@duocuc.cl>
2025-04-09 09:26:47 -0400

    Uso del v-show y otros

--- src/modules/calcular/views/CalcularView.vue
+++ src/modules/calcular/views/CalcularView.vue
@@ -2,16 +2,23 @@
     <h3>Propiedades computasdas</h3>
     <input type="text"  v-model="valor">
     {{ valorCalculado }}
-    <resultadoseccion :resultado="valorCalculado * 5"></resultadoseccion>
+    <resultadoseccion v-if="mostrarElemento" :resultado="valorCalculado "></resultadoseccion>
+    <div v-else-if="isNaN(valor)">Debe ser un numero para poder calcular</div>
+    <div v-else >Aqui no ve muestra nada</div>
 </template>
 
+<!-- a*3*5= a*15 -->
 <script setup>
 import {ref, computed} from 'vue';
-import resultadoseccion from '../components/resultadoseccion.vue';
+import resultadoseccion from '../components/ResultadoSeccion.vue';
 const valor = ref('');
 const valorCalculado = computed(()=>{
-    return valor.value * 2
+    return valor.value * 3
 })
+
+const mostrarElemento = computed(()=>{
+    return valor.value >= 10;
+});
 </script>
 
 <style scoped>
